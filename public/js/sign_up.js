6e0690233e2b447bf28471cbab62d32098b479a5
koundinyaatchyutuni <20131a4407@gmail.com>
2024-09-28 19:57:01 +0530

    done with changes in sign up page

--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -23,7 +23,10 @@ id.addEventListener('input', async function(event) {
     const sign_upButton = document.getElementById("sign_up");
     if (data.available) { //data.available will be true when the user_id is available in data that is no duplicate user_id.
         validate2.innerHTML = "✔️";
+        if (id.value == "") validate2.innerHTML = "";
         validate2.style.color = 'green';
+        user_container.style.border = '2px solid rgb(100, 250, 117)';
+        user_container.style.boxShadow = '2px 2px 5px rgb(113, 242, 184)';
         if (validate1.innerHTML == "✔️") {
             sign_upButton.addEventListener('mouseover', () => {
                 sign_upButton.style.cursor = 'pointer';
@@ -33,9 +36,8 @@ id.addEventListener('input', async function(event) {
     } else {
         validate2.innerHTML = "❌";
         validate2.style.color = 'red';
-        user_container.addEventListener('focus', () => {
-            user_container.style.borderColor = 'red';
-        })
+        user_container.style.borderColor = 'RGB(255, 69, 0)';
+        user_container.style.boxShadow = '1px 1px 1px RGBA(255, 69, 0,0.8)';
         sign_upButton.addEventListener('mouseover', () => {
             sign_upButton.style.cursor = 'not-allowed';
         });
@@ -57,7 +59,10 @@ position.addEventListener('input', async function(event) {
     const sign_upButton = document.getElementById("sign_up");
     if (data.available) {
         validate1.innerHTML = "✔️";
+        if (position.value == "") validate1.innerHTML = "";
         validate1.style.color = 'green';
+        rank_container.style.border = '2px solid rgb(100, 250, 117)';
+        rank_container.style.boxShadow = '2px 2px 5px rgb(113, 242, 184)';
         if (validate2.innerHTML == "✔️") {
             sign_upButton.addEventListener('mouseover', () => {
                 sign_upButton.style.cursor = 'pointer';
@@ -67,9 +72,8 @@ position.addEventListener('input', async function(event) {
     } else {
         validate1.innerHTML = "❌";
         validate1.style.color = 'red';
-        rank_container.addEventListener('focus', () => {
-            rank_container.style.borderColor = 'red';
-        })
+        rank_container.style.borderColor = 'RGB(255, 69, 0)';
+        rank_container.style.boxShadow = '1px 1px 1px RGBA(255, 69, 0,0.8)';
         sign_upButton.addEventListener('mouseover', () => {
             sign_upButton.style.cursor = 'not-allowed';
         });
